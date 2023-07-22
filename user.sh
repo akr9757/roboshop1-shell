1e732e8b8428c6fe219fb08e44c47dc0bb4cf009
akr9757 <anilkumareddy9757@gmail.com>
2023-07-22 18:10:43 +0530

    updated dns for internal communication

--- user.sh
+++ user.sh
@@ -1,19 +1,26 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
 yum install nodejs -y
+
+cp user.service /etc/systemd/system/user.service
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
 useradd roboshop
+
+rm -rf /app
 mkdir /app
+
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+
 cd /app
 unzip /tmp/user.zip
-cd /app
+
 npm install
-
-
-
 
 systemctl daemon-reload
 systemctl enable user
-systemctl start user
+systemctl restart user
 
 yum install mongodb-org-shell -y
 mongo --host mongodb-dev.akrdevopsb72.online </app/schema/user.js
