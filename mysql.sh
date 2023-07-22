45b13743aab1bfc671d652e605b24169b82373c4
akr9757 <anilkumareddy9757@gmail.com>
2023-07-22 18:44:49 +0530

    mysql service

--- mysql.sh
+++ mysql.sh
@@ -1,8 +1,11 @@
 yum module disable mysql -y
 
+cp mysql.repo /etc/yum.repos.d/mongo.repo
 
 yum install mysql-community-server -y
+
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
+
 mysql_secure_installation --set-root-pass RoboShop@1
 mysql -uroot -pRoboShop@1
