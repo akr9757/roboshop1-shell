d2f653059662fe40e1bc98f18d1503505d07119e
akr9757 <anilkumareddy9757@gmail.com>
2023-07-22 17:42:32 +0530

    script of each component

--- mongodb.sh
+++ mongodb.sh
@@ -2,6 +2,8 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 yum install mongodb-org -y
 
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongo.conf
+
 systemctl start mongod
 systemctl enable mongod
 systemctl restart mongod
