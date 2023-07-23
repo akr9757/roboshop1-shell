1e570951387e38e37ed2ded3079e9c9a6829d2d2
akr9757 <anilkumareddy9757@gmail.com>
2023-07-23 06:50:16 +0530

    if condition

--- catalogue.sh
+++ catalogue.sh
@@ -2,35 +2,8 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-
-yum install nodejs -y
-
-cp ${script_path}/catalogue.service /etc/systemd/system/catalogue.service
+component=catalogue
+func_nodejs
 
 
 
-useradd ${app_user}r
-
-rm -rf /app
-mkdir /app
-
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-
-cd /app
-unzip /tmp/catalogue.zip
-
-npm install
-
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl restart catalogue
-
-echo -e "\e[34m>>>>>>>>>>>>>> Copy Mongo Repo <<<<<<<<<<<<\e[0m"
-cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
-
-echo -e "\e[34m>>>>>>>>>>>>>> Install Mongodb Client <<<<<<<<<<<<\e[0m"
-yum install mongodb-org-shell -y
-
-echo -e "\e[34m>>>>>>>>>>>>>> Load App Schema <<<<<<<<<<<<\e[0m"
-mongo --host mongodb-dev.akrdevopsb72.online </app/schema/catalogue.js
--- user.sh
+++ user.sh
@@ -2,29 +2,10 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+component=user
 
-yum install nodejs -y
-
-cp ${script_path}/user.service /etc/systemd/system/user.service
+func_nodejs
 
 cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
-
-useradd ${app_user}
-
-rm -rf /app
-mkdir /app
-
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-
-cd /app
-unzip /tmp/user.zip
-
-npm install
-
-systemctl daemon-reload
-systemctl enable user
-systemctl restart user
-
 yum install mongodb-org-shell -y
 mongo --host mongodb-dev.akrdevopsb72.online </app/schema/user.js
