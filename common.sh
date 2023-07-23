05a9c9132f38821052445e52e7337f312f38758c
akr9757 <anilkumareddy9757@gmail.com>
2023-07-23 07:42:34 +0530

    if condition

--- common.sh
+++ common.sh
@@ -2,47 +2,51 @@ app_user=roboshop
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 
+func_printhead() {
+  echo -e "\e[34m>>>>>>>>>>>>>> $1 <<<<<<<<<<<<\e[0m"
+}
+
 func_schema_setup() {
   if [ "$schema_setup" == "mongo" ]; then
-     echo -e "\e[34m>>>>>>>>>>>>>> Copy Mongo Repo <<<<<<<<<<<<\e[0m"
+     func_printhead "Copy Mongo Repo"
      cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
 
-     echo -e "\e[34m>>>>>>>>>>>>>> Install Mongodb Client <<<<<<<<<<<<\e[0m"
+     func_printhead "Install Mongodb Client"
      yum install mongodb-org-shell -y
 
-     echo -e "\e[34m>>>>>>>>>>>>>> Load App Schema <<<<<<<<<<<<\e[0m"
+     func_printhead "Load App Schema"
      mongo --host mongodb-dev.akrdevopsb72.online </app/schema/${component}.js
   fi
 }
 
 func_nodejs() {
-  echo -e "\e[34m>>>>>>>>>>>>>> Download Nodejs Repos <<<<<<<<<<<<\e[0m"
+  func_printhead "Download Nodejs Repos"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-  echo -e "\e[34m>>>>>>>>>>>>>> Install Nodejs <<<<<<<<<<<<\e[0m"
+  func_printhead "Install Nodejs"
   yum install nodejs -y
 
-  echo -e "\e[34m>>>>>>>>>>>>>> Add Application User <<<<<<<<<<<<\e[0m"
+  func_printhead "Add Application User"
   useradd ${app_user}
 
-  echo -e "\e[34m>>>>>>>>>>>>>> Add Application Directory <<<<<<<<<<<<\e[0m"
+  func_printhead "Add Application Directory"
   rm -rf /app
   mkdir /app
 
-  echo -e "\e[34m>>>>>>>>>>>>>> Download App Content <<<<<<<<<<<<\e[0m"
+  func_printhead "Download App Content"
   curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
 
-  echo -e "\e[34m>>>>>>>>>>>>>> Unzip App Content <<<<<<<<<<<<\e[0m"
+  func_printhead "Unzip App Content"
   cd /app
   unzip /tmp/${component}.zip
 
-  echo -e "\e[34m>>>>>>>>>>>>>> Install Nodejs Dependencies <<<<<<<<<<<<\e[0m"
+  func_printhead "Install Nodejs Dependencies"
   npm install
 
-  echo -e "\e[34m>>>>>>>>>>>>>> Setup Systemd Service <<<<<<<<<<<<\e[0m"
+  func_printhead "Setup Systemd Service"
   cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
 
-  echo -e "\e[34m>>>>>>>>>>>>>> Start ${component} Service <<<<<<<<<<<<\e[0m"
+  func_printhead "Start ${component} Service"
   systemctl daemon-reload
   systemctl enable ${component}
   systemctl restart ${component}
