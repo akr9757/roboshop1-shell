2506e8729d9268ac33c7ccac987b90ed40f83299
akr9757 <anilkumareddy9757@gmail.com>
2023-07-26 11:43:43 +0530

    functions and conditions update

--- common.sh
+++ common.sh
@@ -131,7 +131,7 @@ func_python() {
   func_status_check $?
 
   func_printhead "Update Password System Service File"
-  sed -i -e 's|payment_appuser_password|${payment_appuser_password}' ${script_path}/payment.service &>>$log_file
+  sed -i -e "s|payment_appuser_password|${payment_appuser_password}" ${script_path}/payment.service &>>$log_file
 
   func_systemd_setup
 }
