8591af2367376f9a2cbd6bde5798e2d12323719d
akr9757 <anilkumareddy9757@gmail.com>
2023-08-01 08:42:17 +0530

    my sql correction

--- mysql.sh
+++ mysql.sh
@@ -9,23 +9,23 @@ if [ -z "$mysql_root_password" ]; then
 fi
 
 
-func_print_head "Disable MySQL 8 Version"
+func_printhead "Disable MySQL 8 Version"
 dnf module disable mysql -y &>>$log_file
 func_stat_check $?
 
-func_print_head "Copy MySQL Repo File"
+func_printhead "Copy MySQL Repo File"
 cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
 func_stat_check $?
 
-func_print_head "Install MySQL"
+func_printhead "Install MySQL"
 yum install mysql-community-server -y &>>$log_file
 func_stat_check $?
 
-func_print_head "Start MySQL"
+func_printhead "Start MySQL"
 systemctl enable mysqld &>>$log_file
 systemctl restart mysqld &>>$log_file
 func_stat_check $?
 
-func_print_head "Reset MySQL Password"
+func_printhead "Reset MySQL Password"
 mysql_secure_installation --set-root-pass $mysql_root_password &>>$log_file
 func_stat_check $?
