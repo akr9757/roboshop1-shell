1c6f6b0bc2612d949399cac7dd74a6c2cd46ffb7
akr9757 <anilkumareddy9757@gmail.com>
2023-08-01 08:39:50 +0530

    datbase servers and app servers

--- mysql.sh
+++ mysql.sh
@@ -4,28 +4,28 @@ source ${script_path}/common.sh
 mysql_root_password=$1
 
 if [ -z "$mysql_root_password" ]; then
-  echo input app user password is missing
+  echo Input MySQL Root Password Missing
   exit 1
 fi
 
-func_printhead "Disable Default Mysql Version"
-yum module disable mysql -y &>>$log_file
-func_status_check $?
 
-func_printhead "Copy Mysql Repo"
-cp ${script_path}/mysql.repo /etc/yum.repos.d/mongo.repo &>>$log_file
-func_status_check $?
+func_print_head "Disable MySQL 8 Version"
+dnf module disable mysql -y &>>$log_file
+func_stat_check $?
 
-func_printhead "Install Mysql Client"
+func_print_head "Copy MySQL Repo File"
+cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
+func_stat_check $?
+
+func_print_head "Install MySQL"
 yum install mysql-community-server -y &>>$log_file
-func_status_check $?
+func_stat_check $?
 
-func_printhead "Start Mysql Service"
+func_print_head "Start MySQL"
 systemctl enable mysqld &>>$log_file
 systemctl restart mysqld &>>$log_file
-func_status_check $?
+func_stat_check $?
 
-func_printhead "Update Default User"
-mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$log_file
-mysql -uroot -pRoboShop@1 &>>$log_file
-func_status_check $?
+func_print_head "Reset MySQL Password"
+mysql_secure_installation --set-root-pass $mysql_root_password &>>$log_file
+func_stat_check $?
